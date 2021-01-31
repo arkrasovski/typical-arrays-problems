2a731616d652b6ae7ddedf0a24adca090fd1079f
Artyom <arkrasovski1996@gmail.com>
2021-01-31 12:59:50 +0300

    done this task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    let minEl = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minEl) {
+            minEl = array[i];
+        }
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return minEl;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    let maxEl = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxEl) {
+            maxEl = array[i];
+        }
+    }
+    return maxEl;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+};
